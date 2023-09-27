ab08163590d71d6853369fc9a68726405193da2b
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2023-09-27 14:13:30 +0200

    Commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Pelele
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
