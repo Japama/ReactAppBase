3ee24d012802d81490e5ae87db9bdff6b7ca4a3f
Juan Bautista Valero Carrasco <31925101+Japama@users.noreply.github.com>
2023-11-12 13:03:01 +0100

    Update Header.tsx

--- src/templates/Header.tsx
+++ src/templates/Header.tsx
@@ -61,39 +61,12 @@ function Header() {
               <a className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
                  href="#" onClick={() => { handleClick(); }}
               >
-                {state.username}
-                <p></p>
                 Logout
               </a>
             </li>
           </ul>
         </div>
       </nav>
-
-  // <header className="flex h-20 bg-blue-500 p-4 fixed w-full top-0 z-10">
-  //   <div className="container mx-auto flex justify-between items-center">
-  //     <h1 className="text-white text-2xl font-bold">Sports Guide</h1>
-  //     <h2 className="text-white font-bold">Tu guía del entrenador</h2>
-  //     {state.isLoggedIn ? (
-  //         <div className=' flex  items-center'>
-  //           <h2 className="flex text-white font-bold">Bienvenido {state.username}</h2>
-  //           <button type="button"
-  //                   className="flex border border-red-500 bg-red-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline"
-  //                   onClick={() => {
-  //                     handleClick();
-  //                   }}>
-  //             Logout
-  //           </button>
-  //         </div>
-  //     ) : (
-  //         <button type="button"
-  //                 className="border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline">
-  //           Login
-  //         </button>
-  //     )}
-  //
-  //   </div>
-  // </header>
 )
   ;
 }
